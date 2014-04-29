ad94fd5b474102f000bfefa72fe69a2825c3aa2a
Kory D Kantenga <kory.kantenga@gmail.com>
2014-04-28 20:59:20 -0400

    Corrected

--- PS7_Q2.R
+++ PS7_Q2.R
@@ -11,6 +11,8 @@ library('xtable')
 
 # DGP
 
+set.seed(5485)
+
 z   = rnorm(200)
 e_1 = rnorm(200)
 e_2 = rnorm(200)
@@ -21,18 +23,22 @@ X <- cbind(z+e_1,z+e_2,z+e_3)
 # PCA and EM algorithm
 
 pr.comp  <- princomp(X,center = FALSE)
-estimate <- pr.compS$scores[,1]
-llambda  <- lm(estimate~X-1)
+estimate <- pr.comp$scores[,1]
 
 pr.compST <- princomp(X,center = TRUE)
 estimateST<- pr.compST$scores[,1]
-llambdaST <- lm(estimateST~X-1)
 
-LLAMBDA <- cbind(llambda$coefficients, llambdaST$coefficients)
 CORR    <- cbind(cor(estimate,z),cor(estimateST,z))
 EM      <- ifa.em(X,ni=1)
 
 # Results
-l_lambda    <- xtable(LLAMBDA)
 correlation <- xtable(CORR)
 loadingsEM  <- xtable(EM$H)
+
+lm(X[,1]~estimate-1)
+lm(X[,2]~estimate-1)
+lm(X[,3]~estimate-1)
+
+lm(X[,1]~estimateST-1)
+lm(X[,2]~estimateST-1)
+lm(X[,3]~estimateST-1)
